798421031f0e3e33590d21f37024311b047a9b04
vamsi <vamsikrishna3415@outlook.in>
2020-04-14 11:28:21 +0530

    3 done

--- learn_ruby/01_temperature/temperature.rb
+++ learn_ruby/01_temperature/temperature.rb
@@ -1,1 +1,9 @@
 #write your code here
+
+def ftoc(temp)
+  ((temp - 32) * 5) / 9
+end
+
+def ctof(temp)
+  ((temp.to_f / 5.0) * 9.0) + 32.0
+end
--- learn_ruby/02_calculator/calculator.rb
+++ learn_ruby/02_calculator/calculator.rb
@@ -1,1 +1,33 @@
 #write your code here
+
+def add(a, b)
+  a + b
+end
+
+def subtract(a, b)
+  a - b
+end
+
+def sum(arr)
+  if arr.reduce(:+)
+    arr.reduce(:+)
+  else
+    0
+  end
+end
+
+def multiply(*args)
+  args.reduce(:*)
+end
+
+def power(a, b)
+  a ** b
+end
+
+def factorial(a)
+  if a == 0 || a == 1
+    1
+  else
+    a * factorial(a - 1)
+  end
+end
